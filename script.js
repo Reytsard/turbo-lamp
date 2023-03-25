f93de0b56d1ca7731387c52876c3cd38b53ba447
Ronald <ronaldreyjr061201@gmail.com>
2023-03-25 21:26:02 +0800

    setup for new object

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gameBoard = () => {
     let gameArea = document.querySelector('#gameArea');
-    let gameboard = [null,null,null,null,null,null,null,null,null];
+    let gameboard = ['','','','','','','','',''];
     let turnCount = 1;
     let playerTurn = player1;
+    let tryAgain = false;
     const removeWelcomeDiv = () => {
         console.log('clicked for removeWelDiv');
         const gameDiv = '<div id="game">\n<div id="gameBox">\n<div class="box">\n<button class="open" type="button" value="0"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="1"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="2"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="3"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="4"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="5"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="6"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="7"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="8"></button>\n</div>\n\n</div>\n</div>\n<div class="game-status" data-value="1">Player 1\'s Turn</div>\n';
@@ -37,11 +38,20 @@ const gameBoard = () => {
     const removeGameDiv = () => {
         const endDiv = '<div id="end-status">\n<div class="es-text">Game Over Player 1 Wins</div>\n<div class="es-buttons">\n<button id="try-again" type="button">Try Again</button>\n</div>\n</div>\n'
         gameArea.innerHTML = endDiv;
+        document.querySelector('#try-again').addEventListener('click',restartGame);
+    }
+    const restartGame = () => {
+        console.log('pressed try again');
+        gameboard = [null,null,null,null,null,null,null,null,null];
+        gameArea.innerHTML = '<div id="welcome">\n<div class="greetings">Welcome to test Intro Tic-Tac-Toe</div>\n<div class="game-button-options">\n<button class="start-button" data-value="start">Start</button>\n</div>\n</div>\n</div>';
+        turnCount = 1;
+        playerTurn = player1;
+        inGame = true;
     }
     const addPick = (player, indexValue, gameboard) =>{
         gameboard[indexValue] = player.playerNumber;
     }
-    return {removeWelcomeDiv}
+    return {removeWelcomeDiv, inGame}
 };
 
 const playerFactory = (name, symbol) => {
@@ -56,61 +66,67 @@ const player1 = playerFactory('player1', '1');
 const player2 = playerFactory('player2', '2');
 const gameboard = gameBoard(); 
 const welcomeDiv = gameArea.querySelector('#welcome');
-welcomeDiv.querySelector('.start-button').addEventListener('click',gameboard.removeWelcomeDiv);
+// while(gameboard.inGame){
+    welcomeDiv.querySelector('.start-button').addEventListener('click',gameboard.removeWelcomeDiv);
+// }
+console.log(gameboard.inGame);
 
 
 
 
 
 
-
-// const GameLogic = () => {
-//     /*
-//     1. game start
-//     2.playerSwitch
-//     3.gameEnd
-//     4.showGamecredits
-//      */
-//     const gameArea = document.querySelector('#gameArea');
-//     const gameStart = () => {
-//         const introBox = '<div id="welcome">\n<div class="greetings">Welcome to test Intro Tic-Tac-Toe</div>\n<div class="game-button-options">\n<button class="start-button">Start</button>\n</div>\n</div>';
-//         const gameDiv = '<div id="game">\n<div id="gameBox">\n<div class="box1">\n<button type="button"></button>\n</div>\n<div class="box2">\n<button type="button"></button>\n</div>\n<div class="box3">\n<button type="button"></button>\n</div>\n<div class="box4">\n<button type="button"></button>\n</div>\n<div class="box5">\n<button type="button"></button>\n</div>\n<div class="box6">\n<button type="button"></button>\n</div>\n<div class="box7">\n<button type="button"></button>\n</div>\n<div class="box8">\n<button type="button"></button>\n</div>\n<div class="box9">\n<button type="button"></button>\n</div>\n</div>\n</div>\n';
-//         const addIntroBox = () => gameArea.appendChild(gameStart.introBox);
-//         const removeIntroBox = () => gameArea.removeChild(gameStart.introBox);
-//         const addGameBox = () => gameArea.appendChild(gameStart.gameDiv);
-//         const removeGameBox = () => gameArea.removeChild(gameStart.gameDiv);
-//         return {addIntroBox, removeIntroBox, addGameBox, removeGameBox};
-//     }
-//     const gameStartButton = () => {
-//         gameArea.querySelector('start-button');
-//         gameStartButton.addEventListener('click',() => {
-//             gameArea.removeChild(gameStart.introBox);
-//             gameArea.appendChild(gameStart);
-//         });
-//     }
-//     const playerSwitch = (() => {
-//         let isOngoing = true;
-//         while(isOngoing){
-//             /*                    
-//             if player has placed their pick,
-//             then next player will move next
-
-//              */ 
-//         }
-//     })();
 
 
 //main
+const newGame = gameBoard();
+const player1 = playerFactory(getNameFromInput, "X");
+const player2 = playerFactory(getNameFromInput,"O");
+newGame.showGreetings();
+do{
+    newGame.checkIfSomeoneWins();
+    if(newGame.startIsPressed){
+        newGame.removeInsideGameArea();
+        newGame.addGameBox(player1, player2);
+    }
+    if(newGame.someoneWon || newGame.gameTie){
+        newGame.removeInsideGameArea();
+        newGame.showCredits();
+    }
+}while(newGame.tryAgain);
 
+const gameBoard = () => {
+    const gameArea = document.querySelector('#game-area');
+    let startIsPressed = false;
+    const showGreetings = () => {
+        gameArea.innerHTML = '<div id="welcome">\n<div class="greetings">Welcome to test Intro Tic-Tac-Toe</div>\n<div class="game-button-options">\n<button class="start-button" data-value="start">Start</button>\n</div>\n</div>\n</div>';
+        const startButton = gameArea.querySelector('#start-button');
+        startButton.addEventListener('click', () => {
+            startIsPressed = true;
+        });
+    };
+    const removeInsideGameArea = () => {
+        gameArea.innerHTML = '';
+    };
+    const addGameBox = (player1, player2) => {
+        gameArea.innerHTML = '<div id="game">\n<div id="gameBox">\n<div class="box">\n<button class="open" type="button" value="0"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="1"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="2"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="3"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="4"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="5"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="6"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="7"></button>\n</div>\n<div class="box">\n<button class="open" type="button" value="8"></button>\n</div>\n\n</div>\n</div>\n<div class="game-status" data-value="1">Player 1\'s Turn</div>\n';
+        let boxButtons = gameArea.querySelectorAll('.box button');
+        boxButtons.forEach(button => {
+            button.addEventListener('click',)
+        });
+    }
+    return {showGreetings,removeInsideGameArea};
+}
 /**
- * make that button on start will turn page into a gameBox
+ * make a startdisplay
+ * if clicked
+ * remove greeting
+ * add gamebox
+ * if someone wins{
+ * remove gamebox
+ * show who wins}
+ * else if all boxes played
+ * show its a tie
+ * remove gamebox
+ * add credits
  */
-
-
-// const buttonBoxes = document.querySelectorAll('.box button').forEach(button => {
-    //     button.addEventListener('click', () => {
-//         button.toggleAttribute('disabled');
-//         button.classList.toggle('open');
-//         button.classList.toggle('selected');
-//     });
-// });
